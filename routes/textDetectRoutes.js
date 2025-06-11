8c08f10ad61ca13e92ebb8b798ebfe8c6b4b01b3
Steven Faliszewski <139910217+stevefali@users.noreply.github.com>
2025-06-11 13:51:02 -0700

    Text detection endpoint, preprocess images to handle rotation attribute, and send boundingPoly's and image dimensions in response

--- routes/textDetectRoutes.js
+++ routes/textDetectRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const vision = require("@google-cloud/vision");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
+const sharp = require("sharp");
+const fs = require("fs");
 
 const imageStorage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -15,7 +17,12 @@ const upload = multer({ storage: imageStorage });
 
 const visionClient = new vision.ImageAnnotatorClient();
 
-const testingFile = "./images/testImage.jpg";
+const fetchDetections = async (imageFile) => {
+  const [result] = await visionClient.textDetection(imageFile);
+  return result.textAnnotations;
+};
+
+const testingFile = "./images/testing-sample.jpg";
 
 function setIncomingFileName(req, file, cb) {
   const extension = getExtensionFromFilename(file.originalname);
@@ -28,23 +35,48 @@ function getExtensionFromFilename(filename) {
   return lettersAfterDot ? lettersAfterDot[1] : "";
 }
 
-// Just confirm connection
-router.get("/check", async (req, res) => {
-  try {
-    console.log("Check endpoint reached.");
-
-    res.status(200).json({ message: "Successfully reached check endpoint!" });
-  } catch (e) {
-    return res.status(400).json({ message: "Error calling check endpoint." });
+const getCorrectlyOrientedImage = async (originalFile) => {
+  const originalMetadata = await sharp(originalFile.path).metadata();
+  if (
+    originalMetadata.orientation === 1 ||
+    typeof originalMetadata.orientation === "undefined"
+  ) {
+    return originalFile.path;
   }
-});
 
-// Test uploading images. "testimg" is the same as the "name" field of the form in the frontend.
+  // Produce a replacement file with corrected orientation
+  const correctedOrientationFilePath = `./images/A-${originalFile.filename}`;
+  await sharp(originalFile.path)
+    .rotate()
+    .withMetadata({ orientation: 1 })
+    .toFile(correctedOrientationFilePath);
+
+  // Delete the original file from the filesystem
+  fs.unlink(originalFile.path, (error) => {
+    if (error) {
+      console.log("Error removing unused file ", error);
+    }
+  });
+  return correctedOrientationFilePath;
+};
+
+// Upload an image. "testimg" is the same as the "name" field of the form data in the frontend.
 router.post("/upload", upload.single("testimg"), async (req, res) => {
   try {
     console.log("file: ", req.file);
 
-    res.status(200).json({ message: req.file });
+    const originalFileName = req.file.originalname;
+
+    const filePath = await getCorrectlyOrientedImage(req.file);
+
+    const metadata = await sharp(filePath).metadata();
+    console.log(
+      `originalWidth: ${metadata.width}, originalHeight: ${metadata.height}, ${metadata.orientation}`
+    );
+
+    res
+      .status(200)
+      .json({ message: `Uploaded file: ${originalFileName} to ${filePath}` });
   } catch (e) {
     console.log(e);
     return res.status(400).json({
@@ -52,6 +84,40 @@ router.post("/upload", upload.single("testimg"), async (req, res) => {
     });
   }
 });
+
+// Upload and image and return detection results.
+router.post("/detect", upload.single("testimg"), async (req, res) => {
+  const originalFileName = req.file.originalname;
+
+  try {
+    const filePath = await getCorrectlyOrientedImage(req.file);
+    const metadata = await sharp(filePath).metadata();
+
+    console.log(`Uploaded file: ${originalFileName}} as ${filePath}`);
+
+    const detectionResults = await fetchDetections(filePath);
+
+    console.log(detectionResults);
+
+    res.status(200).json({
+      message: "Success",
+      originalSize: {
+        width: metadata.width,
+        height: metadata.height,
+      },
+      results: detectionResults,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: `Error detecting text for file ${originalFileName}: ${err.message}`,
+    });
+  }
+});
+
+/* 
+Testing 
+*/
 
 // Basic test of text detection
 const doTheTest = async () => {
@@ -62,6 +128,20 @@ const doTheTest = async () => {
   detections.forEach((text) => {
     console.log(text.description);
   });
+  return result;
 };
+
+// Endpoint just for manual text detection testing
+router.get("/test", async (req, res) => {
+  try {
+    const filePath = await getCorrectlyOrientedImage(req.file);
+    const detectionResult = await fetchDetections(filePath);
+
+    res.status(200).json({ message: "success", results: detectionResult });
+  } catch (e) {
+    res.status(400).json({ message: `error detecting text: ${e}` });
+    console.log(e.message);
+  }
+});
 
 module.exports = router;
